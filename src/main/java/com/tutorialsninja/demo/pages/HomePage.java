cc50495d306f6127d8eb0afb01df9238c46b5f61
Henyshah1 <henyshah.qa@gmail.com>
2023-12-05 22:35:03 +0000

    homework

---
+++ src/main/java/com/tutorialsninja/demo/pages/HomePage.java
@@ -0,0 +1,17 @@
+package com.tutorialsninja.demo.pages;
+
+import com.tutorialsninja.demo.utilities.Utility;
+import org.openqa.selenium.By;
+
+public class HomePage extends Utility {
+    By loginLink = By.linkText("Login");
+    By registerLink = By.linkText("Register");
+
+    public void clickOnLoginLink(){
+        clickOnElement(loginLink);
+    }
+    public void registerLink(){
+        clickOnElement(registerLink);
+    }
+}
+
